a4a2e0fa8be572332469add7323cc3db1f67fee4
ArtemGit1 <131163813+ArtemGit1@users.noreply.github.com>
2023-12-13 20:30:37 +0200

    123

--- src/redux/Store/Store.js
+++ src/redux/Store/Store.js
@@ -20,12 +20,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: ['persist/PERSIST'],
-      },
-    }),
+  
 });
 
 
