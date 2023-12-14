9f3f89eaa68867196ae0d16ef76241722d65dcc6
ArtemGit1 <131163813+ArtemGit1@users.noreply.github.com>
2023-12-14 14:16:28 +0200

    123

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addContact } from '../../redux/ContactsSlice/ContactsSlice';
 
 const ContactForm = () => {
   const dispatch = useDispatch();
+  const contacts = useSelector((state) => state.contacts.items);
 
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
@@ -11,16 +12,25 @@ const ContactForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const newContact = { name, number };
 
-    dispatch(addContact(newContact))
-      .then(() => {
-        setName('');
-        setNumber('');
-      })
-      .catch((error) => {
-        console.error('Error adding contact:', error);
-      });
+    const lowerCaseName = name.toLowerCase();
+    const lowerCaseNumber = number.toLowerCase();
+
+
+    const isContactExists = contacts.some(
+      (contact) => contact.name.toLowerCase() === lowerCaseName || contact.number.toLowerCase() === lowerCaseNumber
+    );
+
+    if (isContactExists) {
+
+      console.log('Contact already exists!');
+      return;
+    }
+
+    dispatch(addContact({ name, number }));
+
+    setName('');
+    setNumber('');
   };
 
   return (
@@ -29,12 +39,10 @@ const ContactForm = () => {
         Name:
         <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
       </label>
-      <br />
       <label>
         Number:
         <input type="text" value={number} onChange={(e) => setNumber(e.target.value)} />
       </label>
-      <br />
       <button type="submit">Add Contact</button>
     </form>
   );
